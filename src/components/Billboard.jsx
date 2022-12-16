384f7714eaa51f650a0ab70b30ae9d6b4ee5501c
jekaonemail <jekaonemail@gmail.com>
2022-12-16 17:46:14 +0200

    header and support

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import "../assets/styles/for_components/billboard.scss";
 import Slider from "./Slider";
+import CallbackIcon from "./icons/CallbackIcon";
+import ArrowDown from "./icons/ArrowDown";
+import Support from "./icons/Support";
 
 const Billboard = () => {
   const categories = [
@@ -49,35 +52,64 @@ const Billboard = () => {
       title: "Спорт і захоплення",
     },
 
-    {
-      id: 10,
-      title: "Одяг, взуття та прикраси",
-    },
+    // {
+    //   id: 10,
+    //   title: "Одяг, взуття та прикраси",
+    // },
 
-    {
-      id: 11,
-      title: "Краса та здоров'я",
-    },
+    // {
+    //   id: 11,
+    //   title: "Краса та здоров'я",
+    // },
 
-    {
-      id: 12,
-      title: "Алкогольні напої та продукти",
-    },
+    // {
+    //   id: 12,
+    //   title: "Алкогольні напої та продукти",
+    // },
   ];
 
   return (
     <section className="billboard">
-      <ul className="billboard__categories">
-        {categories &&
-          categories.map((category) => (
-            <li key={category.id}>
-              <Link to="/">
-                <span className="billboard__category-icon"></span>
-                {category.title}
-              </Link>
-            </li>
-          ))}
-      </ul>
+      <div className="billboard__left">
+        <div className="billboard__support">
+          <h5>
+            <strong>
+              <Support />
+              Служба підтримки покупців
+            </strong>
+            <ArrowDown />
+          </h5>
+
+          <div className="billboard__support-content">
+            <p>Виникли питання? Будемо раді на них відповісти!</p>
+
+            <h4>Прийом дзвінків (Пн-Пт) з 9:00-18:00</h4>
+            <div className="billboard__phones-wrapper">
+              <ul>
+                <li>+38 (095) 446-05-37</li>
+                <li>+38 (066) 890-24-65</li>
+              </ul>
+
+              <button className="billboard__callback-button">
+                <CallbackIcon />
+                Передзвоніть мені
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <ul className="billboard__categories">
+          {categories &&
+            categories.map((category) => (
+              <li key={category.id}>
+                <Link to="/">
+                  <span className="billboard__category-icon"></span>
+                  {category.title}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
 
       <Slider />
     </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,66 @@
 import { Link } from "react-router-dom";
 import "../assets/styles/for_components/header.scss";
 
+import Profile from "./icons/Profile";
+import Bookmark from "./icons/Bookmark";
+import Cart from "./icons/Cart";
+import Catalog from "./icons/Catalog";
+import Logotype from "./icons/Logotype";
+import OrderStatus from "./icons/OrderStatus";
+import Search from "./icons/Search";
+import Menu from "./icons/Menu";
+
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__logotype">GIGANT</div>
+      <div className="header__logotype">
+        <Logotype />
+        GIGANT
+      </div>
 
       <nav className="header__navbar">
-        <Link to="/">Головна</Link>
-        <Link to="/">Про Faststore</Link>
-        <Link to="/">Доставка</Link>
-        <Link to="/">Оплата</Link>
-        <Link to="/">Контакти</Link>
+        <button className="header__catalog-button">
+          <Catalog />
+          Каталог
+        </button>
+
+        <button className="header__check-order-status">
+          <OrderStatus />
+          Статус замовлення
+        </button>
+
+        <div className="header__search">
+          <span className="header__search-icon-wrapper">
+            <Search />
+          </span>
+
+          <input type="text" placeholder="Я шукаю.." />
+
+          <button className="header__search-push">Знайти</button>
+        </div>
+
+        {/* <Link to="/">Про Gigant</Link>
+        <Link to="/">Акції</Link>
+        <Link to="/">Новини</Link>
+        <Link to="/">Клієнтам</Link>
+        <Link to="/">Контакти</Link> */}
+
+        <button className="header__menu">
+          <Menu />
+          Відкрити меню
+        </button>
       </nav>
 
       <div className="header__activity">
-        <button className="header__sign-in">Мій профіль</button>
-        <button className="header__saved-products">Закладки</button>
+        <button className="header__sign-in">
+          <Profile />
+        </button>
+        <button className="header__saved-products">
+          <Bookmark />
+        </button>
 
         <button className="header__cart-button" count={3}>
-          Кошик
+          <Cart />
         </button>
       </div>
     </header>
